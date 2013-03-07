88b6ee9a3bf9f37f1d105a5899f1683d09b0973a
John Crossley <hello@phpcodemonkey.com>
2013-03-07 21:54:30 +0000

    changed some of the JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+function scroll_to(object, current) {
+	$('a').removeClass('current'); // Remove all classes and apply current.
+	current.addClass('current'); // Add the current css class to the selected menu item.
+
+	$('html, body').animate({
+		scrollTop: object.offset().top - 200
+	}, 800);
+}
+
 $(document).ready( function() {
+
+	var title = "- clearfix - Web Development Agency";
 
 	var sticky_navigation_offset_top = $('#stickyheader').offset().top;
 
@@ -10,9 +21,6 @@ $(document).ready( function() {
 		if ( scroll_top > sticky_navigation_offset_top ) {
 			$('#stickyheader').addClass('stickyheader');
 		} else {
-			// $('#stickyheader').css({
-			// 	'position': 'relative'
-			// });
 			$('#stickyheader').removeClass();
 		}
 
@@ -21,31 +29,59 @@ $(document).ready( function() {
 	stickynavigation();
 
 	$(window).scroll( function() {
+
+		var scroll_loc = $(window).scrollTop();
+
+		if ( scroll_loc > $('#home').offset().top ) {
+			$('a').removeClass('current');
+			$('a[href=#home]').addClass('current');
+		}
+
+		if ( scroll_loc > $('#work').offset().top ) {
+			$('a').removeClass('current');
+			$('a[href=#work]').addClass('current');
+		}
+
+		if ( scroll_loc > $('#team').offset().top ) {
+			$('a').removeClass('current');
+			$('a[href=#team]').addClass('current');
+		}
+
+		if ( scroll_loc > $('#contact').offset().top ) {
+			$('a').removeClass('current');
+			$('a[href=#contact]').addClass('current');
+		}
+
 		stickynavigation();
 	});
 
 	// The navigation
 	$('a[href=#home]').on('click', function() {
+		document.title = "Home " + title;
 		scroll_to($('#home'), $(this));
 		return false;
 	});
 
 	$('a[href=#work]').on('click', function() {
+		document.title = "Work " + title;
 		scroll_to($('#work'), $(this));
 		return false;
 	});
 
 	$('a[href=#team]').on('click', function() {
+		document.title = "Team " + title;
 		scroll_to($('#team'), $(this));
 		return false;
 	});
 
 	$('a[href=#about]').on('click', function() {
+		document.title = "About " + title;
 		scroll_to($('#about'), $(this));
 		return false;
 	});
 
 	$('a[href=#contact]').on('click', function() {
+		document.title = "Contact " + title;
 		scroll_to($('#contact'), $(this));
 		return false;
 	});
@@ -70,13 +106,4 @@ $(document).ready( function() {
 		  change: false
 		}
 	});
-})
-
-function scroll_to(object, current) {
-	$('a').removeClass('current'); // Remove all classes and apply current.
-	current.addClass('current'); // Add the current css class to the selected menu item.
-
-	$('html, body').animate({
-		scrollTop: object.offset().top-($('header').height()*2)-50
-	}, 600);
-}
+});
